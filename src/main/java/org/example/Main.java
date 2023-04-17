51b9c5f7a79fa23024b60a4a267608623499536d
Marcos12UdeG <isaac.acosta@alumnos.udg.mx>
2023-04-17 14:22:07 -0600

    commit hola1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("HASTA PRONTO");
         System.out.println("BYE");
+        System.out.println("HASTA PRONTO");
     }
 }
